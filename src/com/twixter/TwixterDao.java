acf96a65050b77e55047de8cf30b2b95463e2d0b
Aniket Lawande <anlawande@gmail.com>
2016-03-23 16:27:57 -0400

    Initial commit with working mysql connection

---
+++ src/com/twixter/TwixterDao.java
@@ -0,0 +1,17 @@
+package com.twixter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TwixterDao {
+
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DriverManagerDataSource dataSource) {
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+}
