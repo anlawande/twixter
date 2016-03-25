7fcdcd4c3590b86b134335d0a05a953506e684fe
Aniket Lawande <anlawande@gmail.com>
2016-03-25 13:48:55 -0400

    returning 400 for bad requests and getTweetsForPersonAndFollowers method

--- src/com/twixter/api/TweetController.java
+++ src/com/twixter/api/TweetController.java
@@ -1,8 +1,10 @@
 package com.twixter.api;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,23 +21,55 @@ public class TweetController {
 	@Autowired
 	private TwixterDao dao;
 
-	@RequestMapping(value="/friend")
-	public @ResponseBody List<Tweet> getTweetsForPerson(HttpServletRequest req) {
+	@RequestMapping(value="/self")
+	public @ResponseBody List<Tweet> getTweetsForPerson(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		String personIdParam = req.getParameter("personId");
 		String searchText = req.getParameter("search");
 		
 		int personId = 0;
 		if (personIdParam == null) {
-			//Throw 400 here
+			resp.setStatus(400);
+			resp.getWriter().write("Bad request - Missing personId param");
 			return null;
 		}
 		try {
 			personId = Integer.parseInt(personIdParam);
 		}
 		catch (NumberFormatException nfe) {
-			//Throw 400 here too
+			resp.setStatus(400);
+			resp.getWriter().write("Bad request - Cannot parse personId param");
+			return null;
 		}
 		
 		return dao.getTweetsForPerson(personId);
+	}
+	
+	@RequestMapping(value="/all")
+	public @ResponseBody List<Tweet> getTweetsForPersonAndFollowers(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String personIdParam = req.getParameter("personId");
+		String searchText = req.getParameter("search");
+		
+		int personId = 0;
+		if (personIdParam == null) {
+			resp.setStatus(400);
+			resp.getWriter().write("Bad request - Missing personId param");
+			return null;
+		}
+		try {
+			personId = Integer.parseInt(personIdParam);
+		}
+		catch (NumberFormatException nfe) {
+			resp.setStatus(400);
+			resp.getWriter().write("Bad request - Cannot parse personId param");
+			return null;
+		}
+		
+		//Self tweets
+		List<Tweet> tweets = dao.getTweetsForPerson(personId);
+		
+		//Followers tweets
+		tweets.addAll(dao.getTweetsForFollowers(personId));
+		
+		return tweets;
 	}
 }
